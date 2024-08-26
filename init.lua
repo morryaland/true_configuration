e35cafbd847dda1f4709ff5908baf5a6eb36e93d
morryaland <vozhov.matvey@gmail.com>
2024-08-26 12:43:08 +0300

    update

--- init.lua
+++ init.lua
@@ -1,45 +1,43 @@
 require "paq" {
 	"savq/paq-nvim",
+  'VonHeikemen/lsp-zero.nvim',
 	"neovim/nvim-lspconfig",
 	"hrsh7th/cmp-nvim-lsp",
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
 	"hrsh7th/cmp-cmdline",
 	"hrsh7th/nvim-cmp",
+     'petertriho/cmp-git',
   "hrsh7th/cmp-vsnip",
   "hrsh7th/vim-vsnip",
-  "jose-elias-alvarez/typescript.nvim",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
+  "jackguo380/vim-lsp-cxx-highlight",
+  "m-pilia/vim-ccls",
 	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 	"slugbyte/lackluster.nvim",
 	"nvim-lualine/lualine.nvim",
+  	"pmizio/typescript-tools.nvim",
+	"nvim-lua/plenary.nvim",
 	{ 'nvim-tree/nvim-web-devicons', opt = true },
   { 'nvim-lua/plenary.nvim', opt = true },
 }
 require("lackluster").setup({
 	tweak_syntax = {
         string = "default",
-        -- string = "#a1b2c3", -- custom hexcode
-        -- string = color.green, -- lackluster color
         string_escape = "default",
         comment = "default",
-        builtin = "default", -- builtin modules and functions
+        builtin = "default",
         type = "default",
         keyword = "default",
         keyword_return = "default",
         keyword_exception = "default",
     },
-    -- You can overwrite the following background colors by setting them to one of...
-    --   1) a hexcode like "#a1b2c3" for a custom color
-    --   2) "none" for transparency
-    --   3) "default" or nil will just use whatever lackluster's default is.
     tweak_background = {
-        normal = 'default',    -- main background
-        -- normal = 'none',    -- transparent
-        -- normal = '#a1b2c3',    -- hexcode 
-        -- normal = color.green,    -- lackluster color
-        telescope = 'default', -- telescope
-        menu = 'default',      -- nvim_cmp, wildmenu ... (bad idea to transparent)
-        popup = 'default',     -- lazy, mason, whichkey ... (bad idea to transparent)
+        normal = 'default',
+        -- normal = 'none',
+        telescope = 'default',
+        menu = 'default',
+        popup = 'default',
     },
 })
 vim.cmd.colorscheme("lackluster")
@@ -48,79 +46,70 @@ require('lualine').setup({
 	  theme = "lackluster",
 	},
 })
+local lsp_zero = require('lsp-zero')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
+local lsp_attach = function(client, bufnr)
+  local opts = {buffer = bufnr}
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = capabilities,
+})
 lspconfig.ccls.setup {
   capabilities = capabilities,
   init_options = {
     cache = {
       directory = ".ccls-cache";
     };
+    highlight = {
+      lsRanges = true;
+    }
   }
 }
-lspconfig.autotools_ls.setup {
-  capabilities = capabilities,
-  init_options = {
-    cache = {
-      directory = ".ccls-cache";
-    };
-  }
-}
-lspconfig.tsserver.setup {
-  capabilities = capabilities,
-  init_options = {
-    cache = {
-      directory = ".ccls-cache";
-    };
-  }
-}
-require("typescript").setup({
-  disable_commands = false, -- prevent the plugin from creating Vim commands 
-  debug = false, -- enable debug logging for command 
-  go_to_source_definition = { 
-    fallback = true, -- fall back to standard LSP definition on failure 
-  },
-})
+lspconfig.tsserver.setup { capabilities = capabilities }
+require("typescript-tools").setup {}
+require("cmp_git").setup()
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        vim.fn["vsnip#anonymous"](args.body)
       end,
     },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp_signature_help' },
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+      { name = 'git' },
+    }),
     mapping = cmp.mapping.preset.insert({
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<Tab>'] = cmp_action.tab_complete(),
+      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
-    })
-  })
+})
 vim.wo.number = true
-vim.opt.expandtab = true        -- Convert tabs to spaces
-vim.opt.smarttab = true         -- Insert indentation depending on the context
-vim.opt.smartindent = true       -- Enable smart indentation
-vim.opt.tabstop = 2             -- Number of spaces a <Tab> counts for
-vim.opt.softtabstop = 2         -- Number of spaces a <Tab> counts for while editing
-vim.opt.shiftwidth = 2          -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 5
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.api.nvim_create_augroup('MakefileSettings', {clear = true})
 vim.api.nvim_create_autocmd('FileType', {
     group = 'MakefileSettings',
@@ -129,5 +118,4 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.opt_local.expandtab = true
         vim.opt_local.smarttab = true
     end
-})
-
+});
